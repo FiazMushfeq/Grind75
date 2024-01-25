aebb5a66046ad682f8940495480ac0bed8051f10
Fiaz Mushfeq <fiazmushfeq@live.com>
2024-01-25 12:44:47 -0500

    Finished InsertInterval

--- InsertInterval.py
+++ InsertInterval.py
@@ -1,18 +1,20 @@
 from typing import List
 
 def insert(intervals: List[List[int]], newInterval: List[int]) -> List[List[int]]:
-    left, right = [], []
-    for curr in intervals:
-        # current interval is greater than newInterval
-        if newInterval[0] > curr[1]:
-            left.append(curr)
-        # current interval is less than newInterval
-        elif newInterval[1] < curr[0]:
-            right.append(curr)
-        # merge newInterval with current interval
+    result = []
+    for x in range(len(intervals)):
+        # newInterval does not overlap
+        if newInterval[0] > intervals[x][1]:
+            result.append(intervals[x])
+        # insert newInterval and return rest of intervals
+        elif newInterval[1] < intervals[x][0]:
+            result.append(newInterval)
+            return result + intervals[x:]
+        # newInterval overlaps
         else:
-            newInterval = [min(newInterval[0], curr[0]), max(newInterval[1], curr[1])]
-    return left + right
+            newInterval = [min(intervals[x][0], newInterval[0]), max(intervals[x][1], newInterval[1])]
+    result.append(newInterval)
+    return result
 
 intervals, newInterval = [[1,3],[6,9]], [2,5]
 # Output: [[1,5],[6,9]]
