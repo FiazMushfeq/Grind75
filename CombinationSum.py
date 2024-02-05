bbb51bb02b7f9a8e0f07faf7c8efb9931bd6c9f1
Fiaz Mushfeq <fiazmushfeq@live.com>
2024-02-05 09:51:34 -0500

    Finished CombinationSum.py

--- CombinationSum.py
+++ CombinationSum.py
@@ -1,7 +1,23 @@
 from typing import List
 
 def combinationSum(candidates: List[int], target: int) -> List[List[int]]:
-    return None
+    results = []
+    def backtrack(index, path, target, results):
+        # If target is negative, path is too large
+        if target < 0:
+            return
+        # When target is 0, add current path
+        if target == 0:
+            # Clone path since solution is found
+            results.append(path[:])
+            return
+        # Compute candidates for target
+        for x in range(index, len(candidates)):
+            path.append(candidates[x])
+            backtrack(x, path, target - candidates[x], results)
+            path.pop()
+    backtrack(0, [], target, results)
+    return results
 
 candidates, target = [2,3,6,7], 7
 # Output: [[2,2,3],[7]]
