a1e4aa10182fbd1f1d5ef76c582747dd7fba9bed
FiazMushfeq <fiazmushfeq@live.com>
2024-02-19 00:27:35 -0500

    Finished StringToInteger.py

--- StringToInteger.py
+++ StringToInteger.py
@@ -1,25 +1,35 @@
+import math
+import re
+
 def myAtoi(s: str) -> int:
-    return None
+    # Remove whitespaces
+    s = s.strip()
+    if len(s) == 0:
+        return 0
+    # Check for negative sign and remove from s
+    sign = 1
+    if s[0] == '-':
+        sign = -1
+        s = s[1:]
+    elif s[0] == '+':
+        s = s[1:]
+    # Read in numberical value
+    match = re.search(r"^[\d]+", s)
+    if not match:
+        return 0
+    s = match.group(0)
+    # Return s after converting and applying sign, check size
+    s = int(s) * sign
+    if s < math.pow(-2, 31):
+        return int(math.pow(-2, 31))
+    elif s > (math.pow(2, 31) - 1):
+        return int(math.pow(2, 31) - 1)
+    else:
+        return s
 
-s = "42"
-# Output: 42
-print(myAtoi(s))
-
-s = "   -42"
-# Output: -42
-print(myAtoi(s))
-
-s = "4193 with words"
-# Output: 4193
-print(myAtoi(s))
-
-# Implement the myAtoi(string s) function, which converts a string to a 32-bit signed integer (similar to C/C++'s atoi function).
-
-# The algorithm for myAtoi(string s) is as follows:
-
-# Read in and ignore any leading whitespace.
-# Check if the next character (if not already at the end of the string) is '-' or '+'. Read this character in if it is either. This determines if the final result is negative or positive respectively. Assume the result is positive if neither is present.
-# Read in next the characters until the next non-digit character or the end of the input is reached. The rest of the string is ignored.
-# Convert these digits into an integer (i.e. "123" -> 123, "0032" -> 32). If no digits were read, then the integer is 0. Change the sign as necessary (from step 2).
-# If the integer is out of the 32-bit signed integer range [-231, 231 - 1], then clamp the integer so that it remains in the range. Specifically, integers less than -231 should be clamped to -231, and integers greater than 231 - 1 should be clamped to 231 - 1.
-# Return the integer as the final result.
+# Test cases
+print(myAtoi("42"))  # Output: 42
+print(myAtoi("   -42"))  # Output: -42
+print(myAtoi("4193 with words"))  # Output: 4193
+print(myAtoi("words and 987"))  # Output: 0
+print(myAtoi("-91283472332"))  # Output: -2147483648
